bbe615bb5650396fc2796be6b36c5198fe9c2de2
yusin0624 <cindy950624@gmail.com>
2025-03-16 12:51:24 +0800

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    int x, a, b, c;
+    int x, a, b, C;
     scanf("%d", &x);
     a = x/100;
     x = x-100*a;
     b = x/10;
     x = x-10*b;
-    c = x;
+    C = x;
     printf("%d+%d+%d = %d\n", a, b, c, a+b+c);
     printf("%d*%d*%d = %d\n", a, b, c, a*b*c);
     return 0;
