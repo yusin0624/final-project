9670d59299c613d2c677980980d4cba7f3040261
star-edwin0127 <lutzuwen0127@gmail.com>
2025-03-16 13:07:49 +0800

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    int x, a, b, C;
-    scanf("%d", &x);
-    a = x/100;
-    x = x-100*a;
-    b = x/10;
-    x = x-10*b;
-    C = x;
+    int X, a, b, C;
+    scanf("%d", &X);
+    a = X/100;
+    X = X-100*a;
+    b = X/10;
+    X = X-10*b;
+    C = X;
     printf("%d+%d+%d = %d\n", a, b, c, a+b+c);
     printf("%d*%d*%d = %d\n", a, b, c, a*b*c);
     return 0;
