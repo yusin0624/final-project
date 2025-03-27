bd4518da65c41023607f9d5cf62525c3c68babc4
star-edwin0127 <lutzuwen0127@gmail.com>
2025-03-27 16:12:16 +0800

    Create 1-1.c

---
+++ 1-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int X, a, b, c;
+    scanf("%d", &X);
+    a = X/100;
+    X = X-100*a;
+    b = X/10;
+    X = X-10*b;
+    c = X;
+    printf("%d+%d+%d = %d\n", a, b, c, a+b+c);
+    printf("%d*%d*%d = %d\n", a, b, c, a*b*c);
+    return 0;
+}
