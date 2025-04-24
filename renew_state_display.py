b5a483ea7cbfca0c6bf6dd13b54c7897ad4d43c1
star-edwin0127 <lutzuwen0127@gmail.com>
2025-04-24 16:45:29 +0800

    Update renew_state_display.py
    把boss , 小兵 , player血條顯示分別出來，避免血條重疊，每個y數值都不同。

--- renew_state_display.py
+++ renew_state_display.py
@@ -7,14 +7,35 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+#player draw
 def draw_hp(self , screen , font , x = 50 , y = 50 , spacing=60) :
-    blood_y = y + spacing  #最上面的
     # 畫血條
-    pygame.draw.rect(screen, RED, (x, blood_y, 100, 20))
+    pygame.draw.rect(screen, RED, (x, y, 100, 20))
     hp_ratio = self.hp / self.max_hp if self.max_hp > 0 else 0
-    pygame.draw.rect(screen, GREEN, (x, blood_y, int(100 * hp_ratio), 20))
+    pygame.draw.rect(screen, GREEN, (x, y, int(100 * hp_ratio), 20))
 
     # 顯示 HP 數字
     name_text = font.render(f"{self.name} HP: {self.hp}/{self.max_hp}", True, WHITE)
-    screen.blit(name_text, (x + 110, blood_y))
+    screen.blit(name_text, (x + 110, y))
 
+#monster draw(小兵)
+def draw_hp(self , screen , font , x = 50 , y = 150 , spacing=60) :
+    # 畫血條
+    pygame.draw.rect(screen, RED, (x, y, 100, 20))
+    hp_ratio = self.hp / self.max_hp if self.max_hp > 0 else 0
+    pygame.draw.rect(screen, GREEN, (x, y, int(100 * hp_ratio), 20))
+
+    # 顯示 HP 數字
+    name_text = font.render(f"{self.name} HP: {self.hp}/{self.max_hp}", True, WHITE)
+    screen.blit(name_text, (x + 110, y))
+
+#monster draw(boss)
+def draw_hp(self , screen , font , x = 50 , y = 250 , spacing=60) :
+    # 畫血條
+    pygame.draw.rect(screen, RED, (x, y, 100, 20))
+    hp_ratio = self.hp / self.max_hp if self.max_hp > 0 else 0
+    pygame.draw.rect(screen, GREEN, (x, y, int(100 * hp_ratio), 20))
+
+    # 顯示 HP 數字
+    name_text = font.render(f"{self.name} HP: {self.hp}/{self.max_hp}", True, WHITE)
+    screen.blit(name_text, (x + 110, y))
